c7bac5673c726bbfe28f0d25ee27923a72be9fb7
JuanPastor <jpastorp@unsa.edu.pe>
2022-06-19 16:06:33 -0500

    Si invirtieron los cuadrados con flipV

--- ejemplo.c
+++ ejemplo.c
@@ -5,5 +5,5 @@ void display(){
   char** dos = join(whiteSquare, reverse(whiteSquare));
   char** cuatro = join(dos, dos);
   char** ocho = join(cuatro, cuatro);
-  interpreter(ocho);
+  interpreter(flipV(ocho));
 }
