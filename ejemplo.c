be2140753492ef96ab70da66469387fade820409
JuanPastor <jpastorp@unsa.edu.pe>
2022-06-19 18:00:44 -0500

    Se logro implementar el ajedrez completo

--- ejemplo.c
+++ ejemplo.c
@@ -2,6 +2,12 @@
 #include "figures.h"
 
 void display(){
+
+  char** dos = join(whiteSquare, reverse(whiteSquare));
+  char** cuatro = join(dos, dos);
+  char** ocho = join(cuatro, cuatro);
+  char** dosFilas = up(ocho, flipV(ocho));
+
   char** TorreBlancaTableroNegro = superImpose(rook,reverse(whiteSquare));
   char** CaballoBlancoTableroBlanco = superImpose(knight,whiteSquare);
   char** AlfilBlancoTableroNegro = superImpose(bishop,reverse(whiteSquare));
@@ -17,14 +23,20 @@ void display(){
   char** seisPiezas = join(cincoPiezas, AlfilBlancoTableroBlanco);
   char** sietePiezas = join(seisPiezas, CaballoBlancoTableroNegro);
   char** ochoPiezas = join(sietePiezas, TorreBlancaTableroBlanco);
+  char** PeonBlancoTableroBlanco = superImpose(pawn,whiteSquare);
+  char** PeonBlancoTableroNegro = superImpose(pawn,reverse(whiteSquare));
 
-  /*  
-  char** dos = join(whiteSquare, reverse(whiteSquare));
-  char** cuatro = join(dos, dos);
-  char** ocho = join(cuatro, cuatro);
-  char** dosFilas = up(ocho, flipV(ocho));
-  char** cuatroFilas = up(dosFilas, dosFilas);
-  char** piezas =join(join(join(join(join(join(join(rook, knight),bishop),king),queen),bishop),knight),rook);
-  char** piezasTablero = superImpose(piezas, ocho);*/
-  interpreter(ochoPiezas);
+  char** dosPiezasPeones = join(PeonBlancoTableroBlanco, PeonBlancoTableroNegro);
+  char** cuatroPeones = join(dosPiezasPeones, dosPiezasPeones);
+  char** ochoPeones = join(cuatroPeones, cuatroPeones);
+
+  char** dosFilasPiezas = up(ochoPiezas, ochoPeones);
+  char** dosFilasPiezasAbajo = up(ochoPeones, ochoPiezas);
+  char** cuatroFilasPiezas = up(dosFilasPiezas, reverse(dosFilas));
+
+  char** tableroLleno = up(up(cuatroFilasPiezas,reverse(dosFilas)),reverse(dosFilasPiezasAbajo));
+  
+
+
+  interpreter(tableroLleno);
 }
